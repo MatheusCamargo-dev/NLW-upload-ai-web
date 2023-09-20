f7658bee971c0facac7a7aff4cebe29091d3f45e
MatheusCamargo <matheuscamargodev@gmail.com>
2023-09-20 16:47:25 -0300

    feat: Create Transcription

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,9 +2,11 @@ import fastify from "fastify";
 import { prisma } from "./lib/prisma";
 import { getAllProptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
 
 const app = fastify()
 
+app.register(createTranscriptionRoute)
 app.register(getAllProptsRoute)
 app.register(uploadVideoRoute)
 
